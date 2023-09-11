c3d08fcd9a125fe993efe9b6c42ba89e39c76c0f
dalfreire <danielfreire,net@gmail.com>
2023-09-10 23:09:15 -0300

    exemplo de documentação em Java

---
+++ documentacaoJava/Comentarios.java
@@ -0,0 +1,38 @@
+package documentacaoJava;
+
+public class Comentarios {
+
+    public static void main(String[] args) {
+
+    //Tipos de Comentários
+
+    // 1.ONe Line
+    // Olá, eu sou um comentário em uma única linha (One Line)    
+
+
+    // 2. Multi Line (comentário mais abrangente e robusto/detalhado)
+
+    /* Olá,
+    Eu sou um comentario
+    * que posso ser mais detalhadod
+    * quando necessário
+    */
+        
+    }
+    
+    
+
+    // 3. Documentation
+
+    /** 
+    * Estas duas estrelinhas acima
+    * é para identificar que você
+    * pretende elaborar um comentário
+    * a nível de documentação.
+    * Que incrível !!!
+    */
+
+    public void metodo(){
+
+    }
+}
