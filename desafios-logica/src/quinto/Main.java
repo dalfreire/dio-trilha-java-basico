40c2dc9eb99cc86a356a0b762444879bda19c0db
dalfreire <danielfreire,net@gmail.com>
2023-09-18 15:21:40 -0300

    desafio grande deposito

---
+++ desafios-logica/src/quinto/Main.java
@@ -0,0 +1,26 @@
+package quinto;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        double valor = scanner.nextDouble();
+
+        if (valor > 0) {
+            //TODO: Imprimir a mensagem de sucesso, formatando o saldo atual (vide Exemplos).
+            System.out.println("Deposito realizado com sucesso!");
+            System.out.println("Saldo atual: R$ " + df.format(valor));
+        } else if (valor == 0) {
+            //TODO: Imprimir a mensagem de valor inválido.
+            System.out.println("Encerrando o programa...");
+        } else {
+             //TODO: Imprimir a mensagem de encerrar o programa.
+             System.out.println("Valor invalido! Digite um valor maior que zero.");
+        }
+    }
+}
